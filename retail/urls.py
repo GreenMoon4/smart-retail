e7b46db04f01b8ad5792799e0f1bece74bd5a45e
Josua Blejeru <josua.blejeru@yahoo.de>
2018-10-21 11:51:25 +0200

    add some views for "social, "purchases" and so on.

--- retail/urls.py
+++ retail/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('updates/', views.updates_view, name='updates'),
+    path('promos/', views.promos_view, name='promos'),
+    path('purchases/', views.purchases_view, name='purchases'),
+    path('social/', views.social_view, name='social'),
 ]
