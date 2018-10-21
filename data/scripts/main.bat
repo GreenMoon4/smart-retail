6c1ed473918447e88987dc4a80d79ffd61385a69
Josua Blejeru <josua.blejeru@yahoo.de>
2018-10-21 12:16:31 +0200

    the `retail/update/index.html` will serve the Updates file

--- data/scripts/main.bat
+++ data/scripts/main.bat
@@ -1,1 +1,1 @@
-C:\app\PostgreSQL\11\bin\psql -h localhost -d smart_retail -U postgres -p 5432 -a -q -f ./dataupload.sql
+psql -h localhost -d smart_retail -U postgres -p 5432 -a -q -f ./dataupload.sql
